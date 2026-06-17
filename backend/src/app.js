9fd343cd4589412c31e92f0b2106839f58784940
sujit gore <sujitgore67@gmail.com>
2026-06-17 21:36:38 +0530

    Add dotenv dependency

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,9 +5,12 @@ app.use(express.json());
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
+  origin: [
+    "http://localhost:5173",
+    "https://bookswap.vercel.app"
+  ],
+  credentials: true
+}));
 
 // all routes
 const authRouter = require("./routes/auth.routes"); 
